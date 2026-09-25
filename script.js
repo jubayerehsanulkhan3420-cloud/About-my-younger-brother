2aecfde82f3b0c955b54d24dd676c012ad082a0b
jubayerehsanulkhan3420-cloud <jubayerehsanulkhan3420@gmail.com>
2026-09-25 21:46:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const button =
-  document.getElementById("
+  document.getElementById("OliBtn");
+button.addEventListener("click",function() {
+  alert("Oli is my younger brother . I love him so much. I can do suiside for him. I can not tail that how many love has my heart. But the love is unpredictable. He may not love me.But I have always love for him.I can give my heart for him.If you ask me that, Why. It answer is I dont know");
+});
